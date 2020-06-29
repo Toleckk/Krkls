7ccf678ab7425befa98cd5efb68cf2ba62f50f1e
Toleckk <anatolich1995@gmail.com>
2020-06-29 20:51:11 +0300

    Custom key function added

--- src/ui/List.js
+++ src/ui/List.js
@@ -2,8 +2,8 @@ import React from 'react'
 import styles from './List.module.css'
 import {Item} from './Item'
 
-export const List = ({items}) => (
+export const List = ({items, makeKey = item => item.name}) => (
     <ul className={styles.list}>{
-        items.map(item => <li key={item.name} className={styles.item}><Item item={item}/></li>)
+        items.map(item => <li key={makeKey(item)} className={styles.item}><Item item={item}/></li>)
     }</ul>
 )
