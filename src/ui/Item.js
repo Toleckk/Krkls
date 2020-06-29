25ec3efed71a95839d695f733e5a913b5c3a832b
Toleckk <anatolich1995@gmail.com>
2020-06-29 20:50:48 +0300

    Image size handling added

--- src/ui/Item.js
+++ src/ui/Item.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useMemo, useState, useEffect} from 'react'
 import styles from './Item.module.css'
 
-export const Item = ({item}) => (
-    <button className={styles.item + (item.available ? (' ' + styles.available) : '')} title={item.name}>
-        <img src={item.image || "/images/unknown.png"} alt={item.name} width="100%"/>
-    </button>
-)
+export const Item = ({item}) => {
+    const [image, setImage] = useState(null)
+    const [loaded, setLoaded] = useState(false)
+    const [fit, setFit] = useState(false)
+
+    const src = item.image || '/images/unknown.png'
+
+    useEffect(() => {
+        const img = new Image()
+        img.onload = () => setLoaded(true)
+        img.src = src
+        setImage(img)
+    }, [item.image])
+
+    useEffect(() => setFit(loaded && (image.height < 41 && image.width < 41)), [loaded])
+
+    const style = useMemo(
+        () => ({backgroundImage: `url(${src})`, backgroundSize: fit ? 'auto' : 'contain'}),
+        [item.image, fit]
+    )
+
+    return (
+        <button className={styles.item + (item.available ? (' ' + styles.available) : '')} title={item.name}>{
+            loaded && <div role="img" style={style} className={styles.image}/>
+        }</button>
+    )
+}
