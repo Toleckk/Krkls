c349b4d75c7024310841bc73f77817f12b3eaa08
Toleckk <anatolich1995@gmail.com>
2020-06-29 20:51:43 +0300

    Ships component added

---
+++ src/Ships.js
@@ -0,0 +1,16 @@
+import React, {useMemo, memo} from 'react'
+import {List} from './ui/List'
+import {useShipsContext, sortShips} from './services/ships'
+import styles from './Ships.module.css'
+
+const key = ship => ship.race + ' ' + ship.name
+
+export const Ships = memo(() => {
+    const {ships} = useShipsContext()
+
+    const sortedShips = useMemo(() => sortShips(ships), [ships])
+
+    return <div className={styles.ships}>{
+        sortedShips.map(c => <div className={styles.ships} key={c[0].name}><List items={c} makeKey={key}/></div>)
+    }</div>
+})
