c45188706a93c992533e2aaa249ad8aee3bb3354
Toleckk <anatolich1995@gmail.com>
2020-06-29 20:51:58 +0300

    Ships component injected

--- src/Items.js
+++ src/Items.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import {Devices} from './Devices'
 import {Weapons} from './Weapons'
+import {Ships} from './Ships'
 import styles from './Items.module.css'
 import {Panel} from './ui/Panel'
 
 export const Items = () => (
-    <div className={styles.items}>
-        <div className={styles.column}>
-            <Panel title="Устройства" content={<Devices/>}/>
+    <>
+        <div className={styles.items}>
+            <div className={styles.column}>
+                <Panel title="Устройства" content={<Devices/>}/>
+            </div>
+            <div className={styles.column}>
+                <Panel title="Оружие" content={<Weapons/>}/>
+            </div>
         </div>
         <div className={styles.column}>
-            <Panel title="Оружие" content={<Weapons/>}/>
+            <Panel title="Корабли" content={<Ships/>}/>
         </div>
-    </div>
+    </>
 )
