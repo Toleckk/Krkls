1802fd2114d7ade1c500c4cd5bf9f92d6b00c4d6
Toleckk <anatolich1995@gmail.com>
2020-06-30 01:21:15 +0300

    Moved to CRA

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import './index.css'
+
 
 import App from './App'
 
-const rootElement = document.getElementById('root')
+import * as serviceWorker from './serviceWorker'
+
+
+
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    rootElement,
-)
+
+  <React.StrictMode>
+
+    <App />
+
+  </React.StrictMode>,
+
+  document.getElementById('root')
+
+);
+
+
+serviceWorker.register();
