35d4f4f2740ee46a8dac1e1b188cd9d4c6b78f81
Toleckk <anatolich1995@gmail.com>
2020-06-29 20:48:58 +0300

    ShipsContext injected

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import {Lvl} from './Lvl'
 import {DevicesContextProvider} from './services/devices'
 import {WeaponsContextProvider} from './services/weapons'
 import {Items} from './Items'
+import {ShipsContextProvider} from './services/ships'
 
 export default function App() {
     return (
         <SkillsContextProvider>
             <DevicesContextProvider>
                 <WeaponsContextProvider>
-                    <Lvl/>
-                    <br/>
-                    <br/>
-                    <span>Умения:</span>
-                    <Skills/>
-                    <br/>
-                    <Items/>
+                    <ShipsContextProvider>
+                        <Lvl/>
+                        <br/>
+                        <br/>
+                        <span>Умения:</span>
+                        <Skills/>
+                        <br/>
+                        <Items/>
+                    </ShipsContextProvider>
                 </WeaponsContextProvider>
             </DevicesContextProvider>
         </SkillsContextProvider>
