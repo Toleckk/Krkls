132e8a6f3a2c49bacf1e5148ac3cf0399cd2728a
Toleckk <anatolich1995@gmail.com>
2020-07-01 22:47:35 +0300

    Control buttons injected

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import {Lvl} from './Lvl'
-import {Control} from './Control'
+import {Reset} from './Reset'
+import {Undo} from './Undo'
+import {Redo} from './Redo'
 import styles from './Header.module.css'
 
 export const Header = () => (
     <header className={styles.header}>
         <Lvl/>
-        <Control/>
+        <div>
+            <Undo/>
+            <Reset/>
+            <Redo/>
+        </div>
     </header>
 )
