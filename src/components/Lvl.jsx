fabb8f378192c76a497784da4d1d5d98d5261a5d
Toleckk <anatolich1995@gmail.com>
2020-07-04 21:40:53 +0300

    Lvl UI remade

--- src/components/Lvl.jsx
+++ src/components/Lvl.jsx
@@ -1,29 +1,42 @@
 import React, {useMemo} from 'react'
+import classNames from 'classnames'
 import {useSkillsContext} from '../services/skills'
-import {getExperience} from '../services/lvl'
-
+import {getExperience, short} from '../services/lvl'
+import styles from './Lvl.module.scss'
+import {useHighlightContext} from '../services/highlight'
 
 export const Lvl = () => {
     const {sum, skills} = useSkillsContext()
+    const {highlightedSkills} = useHighlightContext()
 
     const requiredLvl = useMemo(() => (
         skills
-            .filter(skill => skill.required)
-            .map(({required, count}) => required.count > count ? required.count - count : 0)
-            .reduce((acc, cur) => acc + cur, 0) 
-        || undefined
-    ), [skills])
+            .filter(skill => highlightedSkills[skill.name])
+            .map(({count, name}) => highlightedSkills[name] > count ? highlightedSkills[name] - count : 0)
+            .reduce((acc, cur) => acc + cur, 0)
+        || 1
+    ), [skills, highlightedSkills])
 
     const exp = getExperience(sum)
     const requiredExp = getExperience(requiredLvl + sum)
 
+    const style = useMemo(() => ({minWidth: (exp / requiredExp) * 100 + '%'}), [exp, requiredExp])
+
     return (
-        <div>
-            <span>Уровень: {sum}</span>
-            <span style={{color: 'green'}}>{requiredLvl && (' +' + requiredLvl)}</span>
-            &nbsp;
-            <span>Опыт: {exp}</span>
-            <span style={{color: 'green'}}>{requiredLvl && (' +' + (requiredExp - exp))}</span>
+        <div className={styles.column}>
+            <div className={styles.row}>
+                <div>
+                    <span className={styles.lvl}>{sum}</span>
+                    <span className={styles.exp}>{short(exp)}</span>
+                </div>
+                <div>
+                    <span className={styles.exp}>{short(requiredExp)}</span>
+                    <span className={classNames(styles.lvl, styles.required)}>{requiredLvl + sum}</span>
+                </div>
+            </div>
+            <div className={styles.progress}>
+                <div className={styles.has} style={style}/>
+            </div>
         </div>
     )
 }
--- src/services/lvl.js
+++ src/services/lvl.js
@@ -1,3 +1,16 @@
 import lvls from '../data/lvls.json'
 
 export const getExperience = lvl => lvls[lvl]
+
+export const short = n => {
+    if(n < 1000)
+        return n
+
+    const num = ~~(n / 1000)
+    const rest = n % 1000
+
+    if(num < 1000)
+        return num + '.' + Math.round(rest / 10) + 'K'
+
+    return (short(num) + 'K').replace(/KK/g, 'M').replace(/MK/g, 'B').replace(/BK/g, 'T')
+}
