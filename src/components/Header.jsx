4e42e077c7aafb4dca21dbd9ccfd756d995e80f4
Toleckk <anatolich1995@gmail.com>
2020-07-05 20:37:05 +0300

    Control buttons moved to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import {Lvl} from './Lvl'
-import {Reset} from './Reset'
-import {Undo} from './Undo'
-import {Redo} from './Redo'
 import styles from './Header.module.css'
+import {useCanGo} from '../services/history'
+import {ControlButton} from './ControlButton'
+import {useHistory} from 'react-router'
+import {useSkillsContext} from '../services/skills'
 
-export const Header = () => (
-    <header className={styles.header}>
-        <Lvl/>
-        <div className={styles.control}>
-            <Undo/>
-            <Reset/>
-            <Redo/>
-        </div>
-    </header>
-)
+export const Header = () => {
+    const {canGoBack, canGoForward} = useCanGo()
+    const history = useHistory()
+
+    const {reset} = useSkillsContext()
+    const onUndo = useCallback(() => history.goBack(), [history])
+    const onRedo = useCallback(() => history.goForward(), [history])
+
+    return (
+        <header className={styles.header}>
+            <Lvl/>
+            <div className={styles.control}>
+                <ControlButton icon="undo" disabled={!canGoBack} onClick={onUndo} title="Назад"/>
+                <ControlButton onClick={reset} icon="reset" title="Сбросить"/>
+                <ControlButton icon="redo" disabled={!canGoForward} onClick={onRedo} title="Вперёд"/>
+            </div>
+        </header>
+    )
+}
--- src/components/Reset.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {useSkillsContext} from '../services/skills'
-import {ControlButton} from './ControlButton'
-
-export const Reset = () => {
-    const {reset} = useSkillsContext()
-    return <ControlButton onClick={reset} icon="reset" title="Сбросить"/>
-}
