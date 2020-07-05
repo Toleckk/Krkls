f9952d9da99b2d8f4c3a2660ceb4ae56758596c8
Toleckk <anatolich1995@gmail.com>
2020-07-05 21:11:38 +0300

    Copy for desktops added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import {useCanGo} from '../services/history'
 import {ControlButton} from './ControlButton'
 import {useHistory} from 'react-router'
 import {useSkillsContext} from '../services/skills'
+import {useCopyLink} from '../services/copy'
 
 export const Header = () => {
     const {canGoBack, canGoForward} = useCanGo()
@@ -13,11 +14,13 @@ export const Header = () => {
     const {reset} = useSkillsContext()
     const onUndo = useCallback(() => history.goBack(), [history])
     const onRedo = useCallback(() => history.goForward(), [history])
+    const copy = useCopyLink()
 
     return (
         <header className={styles.header}>
             <Lvl/>
             <div className={styles.control}>
+                <ControlButton icon="copy" title="Копировать ссылку" onClick={copy}/>
                 <ControlButton icon="undo" disabled={!canGoBack} onClick={onUndo} title="Назад"/>
                 <ControlButton onClick={reset} icon="reset" title="Сбросить"/>
                 <ControlButton icon="redo" disabled={!canGoForward} onClick={onRedo} title="Вперёд"/>
