dfd5c6692a267ae6dc831755a59275aefd5474ca
Toleckk <anatolich1995@gmail.com>
2020-07-08 14:48:22 +0300

    Drawer fixed

--- src/services/drawer.js
+++ src/services/drawer.js
@@ -1,4 +1,4 @@
-import React, {createContext, useContext, useMemo, useState} from 'react'
+import React, {createContext, useCallback, useContext, useMemo, useState} from 'react'
 
 export const DrawerContext = createContext(null)
 
@@ -6,8 +6,12 @@ export const useDrawer = () => useContext(DrawerContext)
 
 export const DrawerProvider = ({children}) => {
     const [item, setItem] = useState(null)
+    const [opened, setOpened] = useState(false)
 
-    const value = useMemo(() => ({item, setItem}), [item, setItem])
+    const open = useCallback(() => setOpened(true), [setOpened])
+    const close = useCallback(() => setOpened(false), [setOpened])
+
+    const value = useMemo(() => ({item, setItem, open, close, opened}), [item, setItem, opened, open, close])
 
     return <DrawerContext.Provider value={value}>{children}</DrawerContext.Provider>
 }
