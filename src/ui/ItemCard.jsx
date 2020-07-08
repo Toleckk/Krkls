994fdeaf0ee04b90cdace8230f8fb29d3d6c4400
Toleckk <anatolich1995@gmail.com>
2020-07-08 23:54:39 +0300

    ItemCard finished

--- src/ui/ItemCard.jsx
+++ src/ui/ItemCard.jsx
@@ -1,43 +1,61 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styles from './ItemCard.module.scss'
 import {useItemImage} from '../services/image'
+import {ItemSkill} from './ItemSkill'
+import {useHighlightContext} from '../services/highlight'
+import {useSkillsContext} from '../services/skills'
 
 export const ItemCard = ({item}) => {
     const {loaded, sizes} = useItemImage(item, 55)
+    const {highlightSkills, resetSkillsHighlight} = useHighlightContext()
+    const {addForItem, findSkill} = useSkillsContext()
 
-    if(!item)
-        return <></>
+    useEffect(() => {
+        highlightSkills(item)
+        return resetSkillsHighlight
+    }, [item, highlightSkills, resetSkillsHighlight])
 
-    if (!item.info)
-        return <span>Информации пока нет!</span>
+    const count = Object.keys(item.skills).map(key => {
+        const count = findSkill(key).count
+        return item.skills[key] > count ? item.skills[key] - count : 0
+    }).reduce((a, b) => a + b, 0) || undefined
 
     return (
         <div className={styles.modal}>
-            <div className={styles.image}>
-                {loaded && <img src={item.image} alt={item.name} style={sizes}/>}
+            <div className={styles.title}>
+                <div className={styles.image}>
+                    {loaded && <img src={item.image} alt={item.name} style={sizes}/>}
+                </div>
+                <span className={styles.name}>{item.name}</span>
             </div>
-            <span className={styles.title}>{item.name}</span>
+            <span className={styles.divider}/>
             <div className={styles.scrollable}>
+                {
+                    item.info ? (
+                            <table>
+                                <tbody>
+                                {Object.keys(item.info).map(key => (
+                                    <tr key={key}>
+                                        <td>{key}</td>
+                                        <td style={{textAlign: 'end'}}>{item.info[key]}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        )
+                        : <span style={{textAlign: 'center'}}>Информации пока нет!</span>
+                }
+            </div>
+            <span className={styles.divider}/>
+            <div style={{display: 'flex'}}>
                 <table className={styles.table}>
-                    <tbody>
-                    {Object.keys(item.info).map(key => (
-                        <tr key={key}>
-                            <td>{key}</td>
-                            <td style={{textAlign: 'end'}}>{item.info[key]}</td>
-                        </tr>
-                    ))}
-                    </tbody>
+                    <tbody>{
+                        Object.keys(item.skills)
+                            .map(key => ({name: key, count: item.skills[key]}))
+                            .map(skill => <ItemSkill skill={skill} key={skill.name}/>)
+                    }</tbody>
                 </table>
-                <table className={styles.table}>
-                    <tbody>
-                    {Object.keys(item.skills).map(key => (
-                        <tr key={key}>
-                            <td>{key}:</td>
-                            <td>{item.skills[key]}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
+                {count && <button className={styles.apply} onClick={() => addForItem(item)} title="Добавить">+{count}</button>}
             </div>
         </div>
     )
