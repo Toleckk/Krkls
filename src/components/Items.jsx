3d2a4620f7c975b44fcb7ddd8b5c972f9f62b226
Toleckk <anatolich1995@gmail.com>
2020-07-15 16:48:28 +0300

    Lazy loading for Drawer added

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {DrawerProvider} from '../services/drawer'
 import {Panel} from '../ui/Panel'
-import {ItemDrawer} from '../ui/ItemDrawer'
 import styles from './Items.module.css'
 import {Loader} from '../ui/Loader'
 
 const Devices = React.lazy(() => import('./Devices').then(m => ({default: m.Devices})))
 const Weapons = React.lazy(() => import('./Weapons').then(m => ({default: m.Weapons})))
 const Ships = React.lazy(() => import('./Ships').then(m => ({default: m.Ships})))
+const ItemDrawer = React.lazy(() => import('../ui/ItemDrawer').then(m => ({default: m.ItemDrawer})))
 
 const Suspense = ({Component}) => <React.Suspense fallback={<Loader/>} children={<Component/>}/>
 
@@ -24,6 +24,8 @@ export const Items = () => (
         <div className={styles.column}>
             <Panel title="Корабли" content={<Suspense Component={Ships}/>}/>
         </div>
-        <ItemDrawer/>
+        <React.Suspense fallback={<></>}>
+            <ItemDrawer/>
+        </React.Suspense>
     </DrawerProvider>
 )
