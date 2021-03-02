71d5e99203541ef5c6459e6de92410be1904d4fd
Toleckk <anatolich1995@gmail.com>
2021-03-02 15:20:23 +0300

    Implement service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'focus-visible'
 import 'normalize.css'
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from './App';
 import './index.scss';
 
@@ -13,7 +14,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorkerRegistration.register();
+
 reportWebVitals();
