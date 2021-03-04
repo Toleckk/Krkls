747f4dfda083970456642b7dfcd2eb42066fd148
Toleckk <anatolich1995@gmail.com>
2021-03-04 15:45:39 +0300

    Remove useless useClipboard props memoization

--- src/services/copy.js
+++ src/services/copy.js
@@ -1,18 +1,13 @@
-import {useAlert} from './alert'
-import {useCallback, useMemo} from 'react'
+import {useCallback} from 'react'
 import {useLocation} from 'react-router'
 import {useClipboard} from 'use-clipboard-copy'
+import {useAlert} from './alert'
 
 export const useCopyLink = () => {
   const location = useLocation()
   const {open} = useAlert()
 
-  const copyParams = useMemo(
-    () => ({onSuccess: () => open('Ссылка скопирована в буфер обмена!')}),
-    [open],
-  )
-
-  const {copy} = useClipboard(copyParams)
+  const {copy} = useClipboard({onSuccess: () => open('Ссылка скопирована в буфер обмена!')})
 
   return useCallback(() => {
     const url = window.location.origin + location.pathname
