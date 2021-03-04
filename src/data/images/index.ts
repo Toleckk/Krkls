8b9dc223da12655c5547fb5a069859c7abbc73bd
Toleckk <anatolich1995@gmail.com>
2021-03-04 15:43:51 +0300

    Rewrite images indexes with ts

---
+++ src/data/images/index.ts
@@ -0,0 +1,7 @@
+import * as devicesImages from './devices'
+import * as weaponsImages from './weapons'
+import * as shipsImages from './ships'
+
+export const devices: Record<string, string> = devicesImages
+export const weapons: Record<string, string> = weaponsImages
+export const ships: Record<string, string> = shipsImages
