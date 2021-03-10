29f6ad6d0bec48837527e5c15d6e128d916fd26b
Toleckk <anatolich1995@gmail.com>
2021-03-10 21:31:21 +0300

    Rewrite Icon ui component with ts

---
+++ src/ui/Icon/Icon.stories.tsx
@@ -0,0 +1,27 @@
+import {Meta, Story} from '@storybook/react/types-6-0'
+import {Icon, IconProps} from './Icon'
+
+export default {
+  component: Icon,
+  title: 'Icon',
+  argTypes: {
+    fill: {
+      control: {
+        type: 'color',
+      },
+    },
+  },
+  args: {
+    icon: 'reset',
+    fill: '#000000',
+  },
+  parameters: {
+    layout: 'centered',
+  },
+} as Meta
+
+export const Usual: Story<IconProps> = props => (
+  <div style={{width: 100}}>
+    <Icon {...props} />
+  </div>
+)
