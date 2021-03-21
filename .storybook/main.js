b2d93d721b577b2394daa8094d89ad40462e4c93
Toleckk <anatolich1995@gmail.com>
2021-03-21 19:37:31 +0300

    Configure storybook to handle import aliases

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
+const path = require('path')
+const rewireAliases = require('react-app-rewire-aliases')
+const {paths} = require('react-app-rewired')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  webpackFinal: rewireAliases.aliasesOptions({
+    '@krkls': path.resolve(__dirname, '..', `${paths.appSrc}/`),
+  }),
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
 }
